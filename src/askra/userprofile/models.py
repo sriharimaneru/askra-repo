da2cb12232c6db03be14135418ad3adf5b4e7726
Kalyan Varma <kalyan123.india@gmail.com>
2012-10-12 10:17:51 +0530

    Using Django forms for edit profile pages

--- src/askra/userprofile/models.py
+++ src/askra/userprofile/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django import forms
+from django.forms.formsets import formset_factory
 from django.contrib.auth.models import User
 from tag.models import Tag
 from calendar import calendar
@@ -171,3 +173,45 @@ class UserTag(models.Model):
     userprofile = models.ForeignKey(UserProfile)
     tag = models.ForeignKey(Tag)
     status = models.IntegerField("Tag Category", choices=USERTAG_STATUS_OPTIONS,)
+
+class EditProfileBasicForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    course = forms.ChoiceField(choices=[ (branch.id, branch.course) for branch in Branch.objects.all() ])
+    branch = forms.ChoiceField(choices=[ (branch.id, branch.branch) for branch in Branch.objects.all() ])
+    year_of_graduation = forms.IntegerField(min_value=1900)
+    city = forms.CharField(max_length=50)
+    about = forms.CharField()
+
+    #def __init__(self, curr_branch_course, *args, **kwargs):
+    #    super(EditProfileBasicForm, self).__init__(*args, **kwargs)
+
+        #courses = list()
+        #branches = list()
+        #branch_courses = Branch.objects.all()
+        #for branch_course in branch_courses:
+        #    courses.append(branch_course.course)
+        #    if curr_branch_course.course == branch_course.course:
+        #        branches.append(branch_course.branch)
+        #courses = list(set(courses))
+        #branches = list(set(branches))
+
+        #self.fields['course'] = forms.ChoiceField(initial=courses.index(curr_branch_course.course),choices=[(courses.index(course), course) for course in courses])
+        #self.fields['branch'] = forms.ChoiceField(initial=branches.index(curr_branch_course.branch),choices=[(branches.index(branch), branch) for branch in branches])
+
+class EditProfileWeblinksForm(forms.Form):
+    facebook_url = forms.URLField(max_length=100)
+    twitter_url = forms.URLField(max_length=100)
+    linkedin_url = forms.URLField(max_length=100)
+
+class EditProfileEducationForm(forms.Form):
+    college = forms.ChoiceField(choices=[(college.id, college.name) for college in College.objects.all()])
+    degree = forms.ChoiceField(choices=[(degree.id, degree.name) for degree in Degree.objects.all()])
+    branch = forms.ChoiceField(choices=[(branch.id, branch.name) for branch in HigherEducationBranch.objects.all()])
+    year_of_graduation = forms.IntegerField(min_value=1900)
+
+class EditProfileEmploymentForm(forms.Form):
+    employer = forms.ChoiceField(choices=[(employer.id, employer.name) for employer in Employer.objects.all()])
+    designation = forms.ChoiceField(choices=[(designation.id, designation.name) for designation in JobDesignation.objects.all()])
+    domain = forms.ChoiceField(choices=[(domain.id, domain.name) for domain in JobDomain.objects.all()])
+    date_of_joining = forms.DateField()
+    date_of_leaving = forms.DateField()
--- src/askra/userprofile/views.py
+++ src/askra/userprofile/views.py
@@ -1,16 +1,123 @@
 # Create your views here.
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response, redirect, render
 from django.template import RequestContext
 from userprofile.models import *
+from django.http import HttpResponseRedirect
 
 def show_profile(request, profile_id):
     if not profile_id:
-        return render_to_response("error.html")
+        return render_to_response("error.html", RequestContext(request, {}))
 
-    user_profile = UserProfile.objects.get(id=1)
+    user_profile = UserProfile.objects.get(id=profile_id)
     student_section = StudentSection.objects.filter(userprofile = user_profile)[0]
     education_details = HigherEducationDetail.objects.filter(userprofile = user_profile)
     employment_details = EmployementDetail.objects.filter(userprofile = user_profile)
     return render_to_response("view_profile.html", RequestContext(request, {'user_profile': user_profile,
     				 'student_section': student_section, 'education_details':education_details,
     				 'employment_details': employment_details}))
+
+def edit_profile_basic(request, profile_id):
+    if not profile_id:
+       return render_to_response("error.html", RequestContext(request, {}))
+
+    user_profile=UserProfile.objects.get(id=profile_id)
+    student_section=StudentSection.objects.filter(userprofile = user_profile)[0]
+
+    if request.method=='POST':
+    	form = EditProfileBasicForm(request.POST)
+    	if form.is_valid():
+            user_profile.first_name = form.cleaned_data['name'].split()[0]
+            user_profile.last_name = form.cleaned_data['name'].split()[1]
+            student_section.branch.course = (Branch.objects.get(id=form.cleaned_data['course'])).course
+            student_section.branch.branch = form.cleaned_data['branch']
+            student_section.year_of_graduation = form.cleaned_data['year_of_graduation']
+            user_profile.city = City.objects.get(city=form.cleaned_data['city'])
+            user_profile.about = form.cleaned_data['about']
+
+            user_profile.save()
+            student_section.save()
+
+    	    return HttpResponseRedirect('/profile/view/'+profile_id)
+    else:
+        form = EditProfileBasicForm({'name':user_profile.get_full_name(), 'year_of_graduation':student_section.year_of_graduation,
+         'city':user_profile.city, 'about':user_profile.about, })
+
+    return render(request, "edit_profile_basic.html", {'form': form, 'profile_id': profile_id,})
+
+def edit_profile_weblinks(request, profile_id):
+    if not profile_id:
+       return render_to_response("error.html", RequestContext(request, {}))
+
+    user_profile=UserProfile.objects.get(id=profile_id)
+
+    if request.method=='POST':
+        form = EditProfileWeblinksForm(request.POST)
+        if form.is_valid():
+            user_profile.facebook_url = form.cleaned_data['facebook_url']
+            user_profile.twitter_url = form.cleaned_data['twitter_url']
+            user_profile.linked_url = form.cleaned_data['linkedin_url']
+            user_profile.save()
+            return HttpResponseRedirect('/profile/view/'+profile_id)
+    else:
+        form = EditProfileWeblinksForm({'facebook_url':user_profile.facebook_url, 
+            'twitter_url': user_profile.twitter_url, 'linkedin_url': user_profile.linked_url})
+
+    return render(request, "edit_profile_weblinks.html", {'form': form, 'profile_id': profile_id,})
+
+
+def edit_profile_education(request, profile_id):
+    if not profile_id:
+       return render_to_response("error.html", RequestContext(request, {}))
+
+    user_profile=UserProfile.objects.get(id=profile_id)
+    #student_section = StudentSection.objects.filter(userprofile = user_profile)[0]
+    education_details = HigherEducationDetail.objects.filter(userprofile=user_profile)
+    EditProfileEducationFormSet = formset_factory(EditProfileEducationForm, extra=0)
+
+    if request.method == 'POST':
+        formset = EditProfileEducationFormSet(request.POST,request.FILES)
+        if formset.is_valid():
+            for i in range(0,formset.total_form_count()):
+                form = formset.forms[i]
+                education_details[i].college = form.cleaned_data['college']
+                education_details[i].degree = form.cleaned_data['degree']
+                education_details[i].branch = form.cleaned_data['branch']
+                education_details[i].year_of_graduation = form.cleaned_data['year_of_graduation']
+                education_details[i].save()
+            return HttpResponseRedirect('/profile/view/'+profile_id)
+    else:
+        formset = EditProfileEducationFormSet()
+        for i in range(0,len(education_details)):
+            form = EditProfileEducationForm({'year_of_graduation':education_details[i].year_of_graduation})
+            formset.forms.append(form)
+
+    return render(request, "edit_profile_education.html", {'formset':formset, 'profile_id':profile_id, })
+
+
+def edit_profile_employment(request, profile_id):
+    if not profile_id:
+       return render_to_response("error.html", RequestContext(request, {}))
+
+    user_profile=UserProfile.objects.get(id=profile_id)
+    employment_details = EmployementDetail.objects.filter(userprofile = user_profile)
+    EditProfileEmploymentFormSet = formset_factory(EditProfileEmploymentForm, extra=0)
+
+    if request.method == 'POST':
+        formset = EditProfileEmploymentFormSet(request.POST, request.FILES)
+        if formset.is_valid():
+            for i in range(0,formset.total_form_count()):
+                form = formset.forms[i]
+                employment_details[i].employer = form.cleaned_data['employer']
+                employment_details[i].designation = form.cleaned_data['designation']
+                employment_details[i].domain = form.cleaned_data['domain']
+                employment_details[i].date_of_joining = form.cleaned_data['date_of_joining']
+                employment_details[i].date_of_leaving = form.cleaned_data['date_of_leaving']
+                employment_details[i].save()
+            return HttpResponseRedirect('/profile/view/'+profile_id)
+    else:
+        formset = EditProfileEmploymentFormSet()
+        for i in range(0,len(employment_details)):
+            form = EditProfileEmploymentForm({'date_of_joining':employment_details[i].date_of_joining, 'date_of_leaving':employment_details[i].date_of_leaving})
+            formset.forms.append(form)
+
+    return render(request, "edit_profile_employment.html", {'formset':formset, 'profile_id':profile_id, })
